fa9dc68d66054ec5b94fb687c21c551427618f18
MMcClure11 <mcclme02@luther.edu>
2020-07-07 09:34:15 -0500

    adds validations to song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,23 @@
 class Song < ApplicationRecord
+  validates :title, presence: true, uniqueness: true, if: :released_in_same_year?
+  # validates :released, inclusion: { in: %w(true false) }
+  validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year  }, if: :released?
+  validates :artist_name, presence: true
+
+  def released?
+    released == true
+  end
+
+  def released_in_same_year?
+    songs = Song.all
+    songs.each do |song|
+      if song.release_year.to_s.include?(self.release_year.to_s)
+        true
+      else
+        false
+      end
+    end
+  end
+  
+  
 end
