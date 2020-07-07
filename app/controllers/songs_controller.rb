41468425b59fe7f22803db89ae473f1e0b847236
MMcClure11 <mcclme02@luther.edu>
2020-07-07 10:14:21 -0500

    adds create action

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,6 +12,15 @@ class SongsController < ApplicationController
     @song = Song.new
   end
 
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
   def edit
   end
 
