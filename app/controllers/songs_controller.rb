79f15793d7863b8498500ee6506920a0ecbaa13b
MMcClure11 <mcclme02@luther.edu>
2020-07-07 09:46:57 -0500

    adds show action and view and before action

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,18 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show]
 
   def index
     @songs = Song.all
   end
 
+  def show
 
+  end
+
+  private
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
 
 end
