9683d1035f0164c422a2f7ff8c011e1566bfee7c
MMcClure11 <mcclme02@luther.edu>
2020-07-07 10:05:17 -0500

    adds song_params method and update action

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 class SongsController < ApplicationController
-  before_action :set_song, only: [:show, :edit]
+  before_action :set_song, only: [:show, :edit, :update]
 
   def index
     @songs = Song.all
@@ -11,10 +11,23 @@ class SongsController < ApplicationController
   def edit
   end
 
+  def update
+    @song.update(song_params)
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_song
     @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
   end
 
 end
