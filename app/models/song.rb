d80a370a9eb5ded55dee17588db2f6da2f35e71b
MMcClure11 <mcclme02@luther.edu>
2020-07-07 09:37:14 -0500

    refactors validation methods in model class

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,5 @@
 class Song < ApplicationRecord
-  validates :title, presence: true, uniqueness: true, if: :released_in_same_year?
-  # validates :released, inclusion: { in: %w(true false) }
+  validates :title, presence: true, uniqueness: true, if: :released_in_same_year_and_same_artist?
   validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year  }, if: :released?
   validates :artist_name, presence: true
 
@@ -8,14 +7,10 @@ class Song < ApplicationRecord
     released == true
   end
 
-  def released_in_same_year?
+  def released_in_same_year_and_same_artist?
     songs = Song.all
     songs.each do |song|
-      if song.release_year.to_s.include?(self.release_year.to_s)
-        true
-      else
-        false
-      end
+       song.release_year.to_s.include?(self.release_year.to_s) && song.artist_name == self.artist_name
     end
   end
   
