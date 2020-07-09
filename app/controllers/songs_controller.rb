616f58ae5c5cd885ea44e6d3dac3167f43d49ff5
Haloren <mckennakyler@gmail.com>
2020-07-09 11:09:51 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,55 +1,57 @@
 class SongsController < ApplicationController
-    before_action :find_song, only: [:show, :edit, :update, :destroy]
+    # before_action :find_song, only: [:show, :edit, :update, :destroy]
 
     def index
         @songs = Song.all
     end
 
-    def show
-        # @song = Song.find(params[:id])
-    end
+    # def show
+    #     # @song = Song.find(params[:id])
+    # end
 
-    def new
-        @song = Song.new
-    end
+    # def new
+    #     @song = Song.new
+    # end
 
-    def create
-        @song = Song.new(song_params)
+    # def create
+    #     # byebug
+    #     @song = Song.new(song_params)
 
-        if @song.save
-            redirect_to song_path(@song)
-        else
-            render :new
-        end
-    end
+    #     if @song.save
+    #         redirect_to @song #song_path(@song)
+    #     else
+    #         render :new
+    #     end
+    # end
 
-    def edit
-        # @song = Song.find(params[:id])
-    end
+    # def edit
+    #     # @song = Song.find(params[:id])
+    # end
 
-    def update
-        # @song = Song.find(params[:id])
-        if @song.update(song_params)
-            redirect_to song_path(@song)
-        else 
-            render :edit
-        end
-    end
+    # def update
+    #     # @song = Song.find(params[:id])
+        
+    #     if @song.update(song_params)
+    #         redirect_to @song #song_path(@song)
+    #     else 
+    #         render :edit
+    #     end
+    # end
 
-    def destroy
-        # @song = Song.find(params[:id])
-        @song.destroy
-        redirect_to song_path
-    end
+    # def destroy
+    #     # @song = Song.find(params[:id])
+    #     @song.destroy
+    #     redirect_to :controller => 'songs', :action => 'index'
+    # end
 
-    private
+    # private
 
-    def song_params
-        params.require(:song).permit(:title, :artist_name, :release_year, :released, :genre)
-    end
+    # def song_params
+    #     params.require(:song).permit(:title, :artist_name, :release_year, :released, :genre)
+    # end
 
-    def find_song
-        @song = Song.find(params[:id])
-    end
+    # def find_song
+    #     @song = Song.find(params[:id])
+    # end
 
 end
