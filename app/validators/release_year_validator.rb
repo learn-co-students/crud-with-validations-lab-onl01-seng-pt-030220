aa63dc6679fcfc893d644248e8e4a40e9def5db0
Haloren <mckennakyler@gmail.com>
2020-07-09 12:03:46 -0700

    Done.

--- app/validators/release_year_validator.rb
+++ app/validators/release_year_validator.rb
@@ -1,10 +1,10 @@
-# class ReleaseYearValidator < ActiveModel::Validator
-#     def validate(record)
-#         byebug
-#         if !!record.release_year
-#             unless record.release_year <= Date.today.year
-#                 record.errors[:release_year] << "Song released can not be after today's date."
-#             end
-#         end
-#     end
-# end
+class ReleaseYearValidator < ActiveModel::Validator
+    def validate(record)
+
+        if !!record.release_year
+            unless record.release_year <= Date.today.year
+                record.errors[:release_year] << "Song released can not be after today's date."
+            end
+        end
+    end
+end
